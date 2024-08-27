4f4a4c5a0c6a7b56cc353bb826d2167bfcd496de
kien-fit <vankientl99@gmail.com>
2024-08-27 20:51:25 +0700

    created response data

--- src/main/java/com/kienjd/controller/UserController.java
+++ src/main/java/com/kienjd/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.kienjd.controller;
 
+import com.kienjd.dto.response.ResponseFailure;
+import com.kienjd.dto.response.ResponseSuccess;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.kienjd.dto.request.UserRequestDTO;
@@ -14,36 +17,64 @@ import java.util.List;
 public class UserController {
 
     @PostMapping("/")
-    public String addUser(@Valid @RequestBody UserRequestDTO user) {
-        return "User added";
+    public ResponseSuccess addUser(@Valid @RequestBody UserRequestDTO user) {
+        System.out.println("Request add user " + user.getFirstName());
+
+        try {
+            return new ResponseSuccess(HttpStatus.CREATED, "User added successfully,", 1);
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @PutMapping("/{userId}")
-    public String updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
-        System.out.println("Update user");
-        return "User updated";
+    public ResponseSuccess updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
+        System.out.println("Request update userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.ACCEPTED, "User updated successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @PatchMapping("/{userId}")
-    public String updateStatus(@Min(1) @PathVariable int userId,
-                               @RequestParam boolean status) {
-        return "User Status changed";
+    public ResponseSuccess updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
+        System.out.println("Request change status, userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.ACCEPTED, "User's status changed successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
-        return "User deleted";
+    public ResponseSuccess deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
+        System.out.println("Request delete userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.NO_CONTENT, "User deleted successfully");
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @GetMapping("/{userId}")
-    public UserRequestDTO getUser(@PathVariable @Min(1) int userId) {
-        return new UserRequestDTO("Kien", "Jd", "admin@kienjd.vn", "0123456789");
+    public ResponseSuccess getUser(@PathVariable @Min(1) int userId) {
+        System.out.println("Request get user detail, userId=" + userId);
+
+        try {
+            return new ResponseSuccess(HttpStatus.OK, "user", new UserRequestDTO("Kien", "Jd", "admin@kienjd.vn", "0123456789"));
+        } catch (Exception e) {
+            return new ResponseFailure(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @GetMapping("/list")
-    public List<UserRequestDTO> getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
+    public ResponseSuccess getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
                                            @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
-        return List.of(new UserRequestDTO("Kien", "Jd", "admin@kienjd.vn", "0123456789"),
-                new UserRequestDTO("Leo", "Messi", "leomessi@email.com", "0123456456"));
+        return new ResponseSuccess(HttpStatus.OK, "users", List.of(new UserRequestDTO("Kien", "Jd", "admin@kienjd.vn", "0123456789"),
+                new UserRequestDTO("Leo", "Messi", "leomessi@email.com", "0123456456")));
     }
 }
