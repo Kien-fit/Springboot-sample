b2e771b0b2e2805dbbdf0f777e898d9edd5b2b85
kien-fit <vankientl99@gmail.com>
2024-08-27 22:28:45 +0700

    add lombok

--- src/main/java/com/kienjd/exception/ErrorResponse.java
+++ src/main/java/com/kienjd/exception/ErrorResponse.java
@@ -1,7 +1,12 @@
 package com.kienjd.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class ErrorResponse {
 
     private Date timestamp;
@@ -10,43 +15,5 @@ public class ErrorResponse {
     private String error;
     private String message;
 
-    public Date getTimestamp() {
-        return timestamp;
-    }
 
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
