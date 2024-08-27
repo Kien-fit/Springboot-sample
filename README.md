6c3caf5f6fb1a64689dffc8e47a9c4221b0d678a
kien-fit <vankientl99@gmail.com>
2024-08-27 21:42:32 +0700

    Multiple Language = i18n

--- README.md
+++ README.md
@@ -21,7 +21,7 @@
 - Lombok
 
 ---
-## Thêm Lombok Vào Application
+## Multiple Language Trong Spring Boot
 ### Lấy Source Code Về
 - git clone https://github.com/Kien-fit/springboot-sample.git
-- git checkout feature/add-lombok-to-application
+- git checkout feature/i18n-for-restful-api
--- src/main/java/com/kienjd/controller/UserController.java
+++ src/main/java/com/kienjd/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.kienjd.controller;
 
+import com.kienjd.connfiguration.Translator;
 import com.kienjd.dto.response.ResponseData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
@@ -20,25 +21,25 @@ public class UserController {
     @PostMapping("/")
     public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO user) {
         log.info("Request add user = {} {}", user.getFirstName(), user.getLastName());
-        return new ResponseData<>(HttpStatus.CREATED.value(), "User added successfully,", 1);
+        return new ResponseData<>(HttpStatus.CREATED.value(), Translator.toLocale("user.add.success"), 1);
     }
 
     @PutMapping("/{userId}")
     public ResponseData<?> updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
         log.info("Request update userId={}", userId);
-        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User updated successfully");
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), Translator.toLocale("user.update.success"));
     }
 
     @PatchMapping("/{userId}")
     public ResponseData<?> updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
         log.info("Request change status, userId={}", userId);
-        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User's status changed successfully");
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), Translator.toLocale("user.change.success"));
     }
 
     @DeleteMapping("/{userId}")
     public ResponseData<?> deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
         log.info("Request delete userId={}", userId);
-        return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "User deleted successfully");
+        return new ResponseData<>(HttpStatus.NO_CONTENT.value(), Translator.toLocale("user.del.success"));
     }
 
     @GetMapping("/{userId}")
