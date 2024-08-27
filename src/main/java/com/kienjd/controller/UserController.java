5c65b869ffcf3f6345aa7bf558d23ec0f45f6149
kien-fit <vankientl99@gmail.com>
2024-08-27 20:39:42 +0700

    update UserController

--- src/main/java/com/kienjd/controller/UserController.java
+++ src/main/java/com/kienjd/controller/UserController.java
@@ -15,34 +15,39 @@ public class UserController {
 
     @PostMapping("/")
     public String addUser(@Valid @RequestBody UserRequestDTO user) {
+        System.out.println("Request add user " + user.getFirstName());
         return "User added";
     }
 
     @PutMapping("/{userId}")
     public String updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
-        System.out.println("Update user");
+        System.out.println("Request update userId=" + userId);
         return "User updated";
     }
 
     @PatchMapping("/{userId}")
     public String updateStatus(@Min(1) @PathVariable int userId,
                                @RequestParam boolean status) {
+        System.out.println("Request change status, userId=" + userId);
         return "User Status changed";
     }
 
     @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
+        System.out.println("Request delete userId =" + userId);
         return "User deleted";
     }
 
     @GetMapping("/{userId}")
     public UserRequestDTO getUser(@PathVariable @Min(1) int userId) {
+        System.out.println("Request get user detail, userId=" + userId);
         return new UserRequestDTO("Kien", "Jd", "admin@kienjd.vn", "0123456789");
     }
 
     @GetMapping("/list")
     public List<UserRequestDTO> getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
                                            @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
+        System.out.println("Request get all of users");
         return List.of(new UserRequestDTO("Kien", "Jd", "admin@kienjd.vn", "0123456789"),
                 new UserRequestDTO("Leo", "Messi", "leomessi@email.com", "0123456456"));
     }
